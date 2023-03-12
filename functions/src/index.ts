99744ff8d49c1a76ea7e395d4ebc3568e1e257b6
POCHI-CUTE <takaono1789@gmail.com>
2023-03-12 14:34:07 +0900

    firestore to

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,74 +2,84 @@ import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 import { Octokit } from 'octokit'
 import { Buffer } from 'node:buffer'
+import dayjs from 'dayjs'
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-// CI/CD導入 github actions
-//
-exports.postContent = functions.https.onRequest(async (req, res) => {
-  // 認証APIKEYの比較
-  const signature = req.get('Authorization')
-  const ghkey: string = process.env.GIHHUBSKEY!
+admin.initializeApp()
+const octokit = new Octokit()
 
-  if (signature !== ghkey) {
-    functions.logger.error('api key did not match')
-    res.status(403).send('Your access is not permitted')
-    return
+interface BlogRequest extends functions.Request {
+  body: {
+    [key: string]: string
   }
+}
 
-  const octokit = new Octokit()
+// Realtime Databaseに書き込みを行う関数
+export const createArticle = functions.https.onRequest(
+  async (req: BlogRequest, res: functions.Response) => {
+    try {
+      // 認証APIKEYの比較
+      const signature = req.get('Authorization')
+      const ghkey: string = process.env.GIHHUBSKEY!
 
-  // 初期化
-  admin.initializeApp()
-  const fsCollect = admin.firestore().collection('articles')
-
-  for (const key of Object.keys(req.body)) {
-    const fileData = await octokit.request(
-      'GET /repos/POCHI-CUTE/nuxt3-blog-contents/contents/{path}',
-      {
-        path: req.body[key as keyof object],
+      if (signature !== ghkey) {
+        functions.logger.error('api key did not match')
+        res.status(403).send('Your access is not permitted')
       }
-    )
 
-    const fileText = Buffer.from(
-      fileData.data.content,
-      fileData.data.encoding
-    ).toString()
-    const title = fileText.split(/\r\n|\r|\n/)[1].replace(/title: /g, '')
-    const titleSnap = await fsCollect
-      .where('title', '==', title)
-      .select('title')
-      .get()
+      for (const key of Object.keys(req.body)) {
+        const filename = req.body[key]
+        // .yamlを編集したときは飛ばす
+        if (isYaml(filename)) continue
 
-    // TODO: updateTimeも入れる。
-    const flagObj = (() => {
-      for (const doc of titleSnap.docs) {
-        if (doc.data().title === title) {
-          return { flag: true, docId: doc.id }
+        const fileData = await octokit.request(
+          'GET /repos/POCHI-CUTE/nuxt3-blog-contents/contents/{path}',
+          {
+            path: filename,
+          }
+        )
+        const fileText = Buffer.from(
+          fileData.data.content,
+          fileData.data.encoding
+        ).toString()
+        // DBに入れるkey
+        const titleId = filename.replace(/\.[^/.]+$/, '')
+        const title = fileText.split(/\r\n|\r|\n/)[1].replace(/title: /g, '')
+        const content = fileText.split('\n').slice(4).join('\n')
+
+        const articleSnapshot = await admin
+          .database()
+          .ref('articles')
+          .orderByChild('titleId')
+          .equalTo(titleId)
+          .once('value')
+        if (articleSnapshot.exists()) {
+          // 既存の記事を更新する
+          const updatedDate = dayjs().format('YYYY-MM-DD')
+          const articleRef = admin
+            .database()
+            .ref('articles')
+            .child(Object.keys(articleSnapshot.val())[0])
+          await articleRef.update({
+            titleId,
+            title,
+            content,
+            updatedDate,
+          })
+        } else {
+          // 新しい記事を追加する
+          const createdDate = dayjs().format('YYYY-MM-DD')
+          const articleRef = admin.database().ref('articles').push()
+          await articleRef.set({ titleId, title, content, createdDate })
         }
       }
-      return { flag: false, docId: '' }
-    })()
 
-    if (flagObj.flag) {
-      fsCollect.doc(flagObj?.docId).update({
-        updateTime: new Date(),
-        text: fileText,
-      })
-      console.log('success update')
-    } else {
-      fsCollect
-        .add({
-          postTime: new Date(),
-          tags: '',
-          text: fileText,
-          title: title,
-        })
-        .catch((err) => {
-          functions.logger.error('add data failed')
-        })
+      res.send({ success: true })
+    } catch (err: any) {
+      console.error(err.message)
+      res.sendStatus(500)
     }
   }
-  res.end()
-})
+)
+function isYaml(filename: string): boolean {
+  return /\.(yaml|yml)$/i.test(filename)
+}
